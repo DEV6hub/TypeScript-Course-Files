1ac12320417572d8e1cf32cfda1b0e7808be7e9a
Andrew Smith <andrews@dev6.com>
2016-07-11 01:53:26 -0400

    Exercise - Unit04 - Classes

--- foodie/scripts/script.ts
+++ foodie/scripts/script.ts
@@ -1,15 +1,57 @@
-function greeter(greeting: string): string {
-  return 'Hello, ' + greeting;
-}
-
 const x: string = 'World';
-const recipeBook: any[] = [];
-
 const list: HTMLElement = document.getElementById('list');
 const nameEl: HTMLInputElement = document.getElementById('newRecipe') as HTMLInputElement;
 const linkEl: HTMLInputElement = document.getElementById('newLink') as HTMLInputElement;
 
-list.innerHTML = (greeter(x));
+class Recipe {
+  constructor(
+    public title: string,
+    public link: string,
+    public completed: boolean = false
+  ) { }
+
+  displayRecipe(): string {
+    return `<a href="${this.link}" target="_blank">${this.title}</a>`;
+  }
+}
+
+class RecipeBook {
+  private list: Recipe[] = [];
+
+  add(recipe: Recipe): void {
+    this.list.push(recipe);
+  }
+
+  displayRecipes(): string[] {
+    return this.list.map((recipe: Recipe, index: number) => {
+      return `
+        <li>
+          ${recipe.displayRecipe()}
+          <div id="checkbox-delete">
+            <input type="checkbox" id="recipeDone">
+            <button id="deleteRecipe" class="delete" data-id="${index}">Delete</button>
+          </div>
+        </li>
+      `;
+    });
+  }
+}
+
+
+const recipeBook: RecipeBook = new RecipeBook();
+recipeBook.add(new Recipe('Simple Pan-Seared Salmon', 'http://www.thekitchn.com/recipe-simple-pan-seared-salmon-recipes-from-the-kitchn-214529'));
+
+function render(): void {
+  list.innerHTML = recipeBook.displayRecipes().join(' ');
+}
+
+render();
+
+// function greeter(greeting: string): string {
+//   return 'Hello, ' + greeting;
+// }
+
+// list.innerHTML = (greeter(x));
 nameEl.value = 'Simple Pan-Seared Salmon';
 linkEl.value = 'http://www.thekitchn.com/recipe-simple-pan-seared-salmon-recipes-from-the-kitchn-214529';
 
