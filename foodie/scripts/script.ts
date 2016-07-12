589516f4806ba16ee41f5705c93231da88435978
Andrew Smith <andrews@dev6.com>
2016-07-11 21:45:14 -0400

    Exercise - Unit06 - Generators

--- foodie/scripts/script.ts
+++ foodie/scripts/script.ts
@@ -1,21 +1,29 @@
-const x: string = 'World';
+// const x: string = 'World';
+const mockTitle: string = 'Simple Pan-Seared Salmon';
+const mockLink: string = 'http://www.thekitchn.com/recipe-simple-pan-seared-salmon-recipes-from-the-kitchn-214529';
 const list: HTMLElement = document.getElementById('list');
 const nameEl: HTMLInputElement = document.getElementById('newRecipe') as HTMLInputElement;
 const linkEl: HTMLInputElement = document.getElementById('newLink') as HTMLInputElement;
 const submitBtn: HTMLElement = document.getElementById('submitRecipe');
+
+function recipeDefaults(title: string, link: string): (target: Function) => void {
+  return function(target: Function): void {
+    target.prototype.title = title;
+    target.prototype.link = link;
+  };
+}
+
 
 interface IRecipe {
   title: string;
   link: string;
 }
 
-
+@recipeDefaults(mockTitle, mockLink)
 class Recipe implements IRecipe {
-  constructor(
-    public title: string,
-    public link: string,
-    public completed: boolean = false
-  ) { }
+  title: string;
+  link: string;
+  completed: boolean = false;
 
   displayRecipe(): string {
     return `<a href="${this.link}" target="_blank">${this.title}</a>`;
@@ -42,11 +50,15 @@ class RecipeBook {
       `;
     });
   }
+
+  remove(index: number): void {
+    this.list.splice(index, 1);
+  }
 }
 
 
 const recipeBook: RecipeBook = new RecipeBook();
-recipeBook.add(new Recipe('Simple Pan-Seared Salmon', 'http://www.thekitchn.com/recipe-simple-pan-seared-salmon-recipes-from-the-kitchn-214529'));
+recipeBook.add(new Recipe());
 
 function render(): void {
   list.innerHTML = recipeBook.displayRecipes().join(' ');
@@ -54,14 +66,24 @@ function render(): void {
 
 render();
 
-submitBtn.addEventListener('click', (ev: MouseEvent): any => {
+submitBtn.addEventListener('click', (ev: MouseEvent): void => {
   ev.preventDefault();
   const title: string = nameEl.value;
   const link: string = linkEl.value;
-  recipeBook.add(new Recipe(title, link));
+  const recipe: Recipe = new Recipe();
+  recipe.title = title;
+  recipe.link = link;
+  recipeBook.add(recipe);
   render();
   nameEl.value = '';
   linkEl.value = '';
+});
+
+list.addEventListener('click', (ev: any) => {
+  if (ev.target && ev.target.matches('.delete')) {
+    recipeBook.remove(ev.target.dataset.id);
+    render();
+  }
 });
 
 // function greeter(greeting: string): string {
@@ -102,7 +124,7 @@ const {firstName: husband, spouse: {firstName: wife}, lastName } = cook;
 
 console.log(`${husband} and ${wife} ${lastName}`);
 
-const timeDelay: Promise<string> = new Promise<string>((resolve, reject) => {
+const timeDelay: Promise<string> = new Promise<string>((resolve: (value?: string) => void, reject: (reason?: any) => void) => {
   console.log('Inside the promise');
   setTimeout(() => resolve('Outside the promise'), 1000);
 });
